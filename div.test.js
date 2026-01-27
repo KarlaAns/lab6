f2f2ebe9513a1aa284cb0b864692de98be8f4c9d
KarlaAns <kansatunauemee@gmail.com>
2026-01-27 12:22:13 -0500

    changes

---
+++ div.test.js
@@ -0,0 +1,7 @@
+const div= require('./div');
+test('Division de 8/2 debe ser 4', () => {
+    expect(div(8, 2)).toBe(4);
+});
+test('Division por cero debe lanzar un error', () => {
+    expect(() => div(8, 0)).toThrow('Division by zero is not allowed.');
+});
